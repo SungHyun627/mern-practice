bf2fd03c5f9404ce7bc029c1d56b337586c35c17
Sunghyun627 <kksscc4@gmail.com>
2022-05-26 18:01:31 +0900

    chore: connect to DB

--- server/index.js
+++ server/index.js
@@ -2,9 +2,21 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
-const app = express;
+dotenv.config();
 
+const app = express();
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
+
+const CONNECTION_URL = `mongodb+srv://${process.env.MONGO_ID}:${process.env.MONGO_PASSWORD}@cluster0.kdon4pj.mongodb.net/?retryWrites=true&w=majority`;
+const PORT = process.env.PORT || 4000;
+
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((err) => console.log(err));
