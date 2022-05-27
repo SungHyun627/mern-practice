33c3850553084e0bd9752fe73bd503b93e0935f4
Sunghyun627 <kksscc4@gmail.com>
2022-05-27 15:40:15 +0900

    feat: create and styling appbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,26 @@
+import React from 'react';
+import { Container, AppBar, Typography } from '@material-ui/core';
+import memories from './images/memories.png';
+import useStyles from './styles';
+
 const App = () => {
+  const classes = useStyles();
+
   return (
     <div>
-      <h1>App</h1>
+      <Container maxwidth="lg">
+        <AppBar className={classes.appBar} position="static" color="inherit">
+          <Typography className={classes.heading} variant="h2" algin="center">
+            Memories
+          </Typography>
+          <img
+            className={classes.image}
+            src={memories}
+            alt="memories"
+            height="60"
+          />
+        </AppBar>
+      </Container>
     </div>
   );
 };
