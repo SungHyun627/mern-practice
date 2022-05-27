d03df54322cd4121f6abeac9fca46b17da2629ad
Sunghyun627 <kksscc4@gmail.com>
2022-05-27 16:14:02 +0900

    feat: create post/posts components

---
+++ client/src/components/Posts/Post/Post.js
@@ -0,0 +1,11 @@
+import useStyles from './styles';
+
+const Post = () => {
+  return (
+    <>
+      <h1>POST</h1>
+    </>
+  );
+};
+
+export default Post;
---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,14 @@
+import useStyles from './styles';
+import Post from './Post/Post';
+
+const Posts = () => {
+  return (
+    <>
+      <h1>POSTS</h1>
+      <Post />
+      <Post />
+    </>
+  );
+};
+
+export default Posts;
