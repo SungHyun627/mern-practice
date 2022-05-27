f9bd7c5452e61f202c62c2e92fa978de5398c6c4
Sunghyun627 <kksscc4@gmail.com>
2022-05-27 13:24:12 +0900

    feat: create controller logic for getposts/createUser

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,8 @@
 import express from 'express';
+import { getPosts, createPost } from '../controllers/posts';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Working');
-});
-
-export default routers;
+router.get('/', getPosts);
+router.post('/', createPost);
+export default router;
