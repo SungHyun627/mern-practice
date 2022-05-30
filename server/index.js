14803c682cf48017913bf95554b28f2b4610c272
Sunghyun627 <kksscc4@gmail.com>
2022-05-30 13:09:45 +0900

    change the code line order to fix cors error

--- server/index.js
+++ server/index.js
@@ -6,12 +6,12 @@ import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 
 dotenv.config();
-
 const app = express();
-app.use('/posts', postRoutes);
+
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = `mongodb+srv://${process.env.MONGO_ID}:${process.env.MONGO_PASSWORD}@cluster0.kdon4pj.mongodb.net/?retryWrites=true&w=majority`;
 const PORT = process.env.PORT || 4000;
