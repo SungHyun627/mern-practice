09090bf09625dade8968364d705c4c3c82b48c3b
Sunghyun627 <kksscc4@gmail.com>
2022-06-02 10:12:53 +0900

    delete unnecessary part realated to heroku

--- server/index.js
+++ server/index.js
@@ -12,9 +12,6 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 app.use('/posts', postRoutes);
-app.get('/', (req, res) => {
-  res.send('Hello Memories API');
-});
 
 const CONNECTION_URL = `mongodb+srv://${process.env.MONGO_ID}:${process.env.MONGO_PASSWORD}@cluster0.kdon4pj.mongodb.net/?retryWrites=true&w=majority`;
 const PORT = process.env.PORT || 4000;
