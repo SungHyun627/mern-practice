8f3262101afd5abf69ecf013a2361925f7eaef16
Sunghyun627 <kksscc4@gmail.com>
2022-07-10 14:36:16 +0900

    feat: set the draft pagination and revise router

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,25 @@ import { Container } from '@material-ui/core';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import Auth from './components/Auth/Auth';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import PostDetails from './components/PostDetails/PostDetails';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 const App = () => {
+  const user = JSON.parse(localStorage.getItem('profile'));
+
   return (
     <BrowserRouter>
-      <Container maxwidth="lg">
+      <Container maxwidth="xl">
         <Navbar />
         <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/auth" exact element={<Auth />} />
+          <Route path="/" element={<Navigate replace to="/posts" />} />
+          <Route path="/posts" element={<Home />} />
+          <Route path="/posts/search" element={<Home />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route
+            path="/auth"
+            element={!user ? <Auth /> : <Navigate replace to="/posts" />}
+          />
         </Routes>
       </Container>
     </BrowserRouter>
