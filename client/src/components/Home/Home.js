251b0ebfa999fb6cbb1a0ce4ec84ec3ec85490a4
Sunghyun627 <kksscc4@gmail.com>
2022-07-10 21:05:03 +0900

    feat: create search bar and styling

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,33 +1,68 @@
-import { Container, Grid, Grow, Paper } from '@material-ui/core';
+import {
+  Container,
+  Grid,
+  Grow,
+  Paper,
+  AppBar,
+  TextField,
+  Button,
+} from '@material-ui/core';
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
 import { useDispatch } from 'react-redux';
 import { getPosts } from '../../actions/posts';
 import { useState, useEffect } from 'react';
 import Pagination from '../Pagination';
+import ChipInput from 'material-ui-chip-input';
+import { useLocation, useNavigate } from 'react-router-dom';
+import useStyles from './styles';
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
 
 const Home = () => {
   const [currentId, setCurrentId] = useState(0);
   const dispatch = useDispatch();
+  const query = useQuery();
+  const navigate = useNavigate();
+  const page = query.get('page') || 1;
+  const searchQuery = query.get('searchQuery');
+  const classes = useStyles();
 
   useEffect(() => {
     dispatch(getPosts());
   }, [currentId, dispatch]);
   return (
     <Grow in>
-      <Container>
+      <Container maxWidth="xl">
         <Grid
           container
           justifyContent="space-between"
           alignItems="stretch"
           spacing={3}
+          className={classes.gridContainer}
         >
-          <Grid item xs={12} sm={7}>
+          <Grid item xs={12} sm={6} md={9}>
             <Posts setCurrentId={setCurrentId} />
           </Grid>
-          <Grid item xs={12} sm={4}>
+          <Grid item xs={12} sm={6} md={3}>
+            <AppBar
+              className={classes.appBarSearch}
+              position="static"
+              color="inherit"
+            >
+              <TextField
+                name="search"
+                variant="outlined"
+                label="Search Memories"
+                fullWidth
+                value="TEST"
+                onChange={() => {}}
+              />
+            </AppBar>
             <Form currentId={currentId} setCurrentId={setCurrentId} />
-            <Paper elevation={6}>
+            <Paper className={classes.pagination} elevation={6}>
               <Pagination />
             </Paper>
           </Grid>
