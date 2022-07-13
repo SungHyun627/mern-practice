ae8c2e62abdeb62b9ddec5eb45eef10b5fd69cd6
Sunghyun627 <kksscc4@gmail.com>
2022-07-13 17:50:35 +0900

    feat: implement endloading and startloading

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -4,16 +4,28 @@ import {
   CREATE,
   UPDATE,
   DELETE,
+  START_LOADING,
+  END_LOADING,
 } from '../constants/actionTypes';
 
-const posts = (state = [], action) => {
+export default (state = { isLoading: true, posts: [] }, action) => {
   switch (action.type) {
+    case START_LOADING:
+      return { ...state, isLoading: true };
+    case END_LOADING:
+      return { ...state, isLoading: false };
     case DELETE:
-      return state.filter((post) => post._id !== action.payload);
+      return {
+        ...state,
+        posts: state.posts.filter((post) => post._id !== action.payload),
+      };
     case UPDATE:
-      return state.posts.map((post) =>
-        post._id === action.payload._id ? action.payload : post
-      );
+      return {
+        ...state,
+        posts: state.posts.map((post) =>
+          post._id === action.payload._id ? action.payload : post
+        ),
+      };
     case FETCH_ALL:
       return {
         ...state,
@@ -24,10 +36,8 @@ const posts = (state = [], action) => {
     case FETCH_BY_SEARCH:
       return { ...state, posts: action.payload };
     case CREATE:
-      return [...state, action.payload];
+      return { ...state, posts: action.payload };
     default:
       return state;
   }
 };
-
-export default posts;
