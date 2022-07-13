5bb2ca95c34b1df1f9fb609db67ff1516fad3700
Sunghyun627 <kksscc4@gmail.com>
2022-07-13 17:55:21 +0900

    resolve merge conflict

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -8,11 +8,7 @@ import {
   END_LOADING,
 } from '../constants/actionTypes';
 
-<<<<<<< HEAD
-const posts = (state = { posts: [] }, action) => {
-=======
 export default (state = { isLoading: true, posts: [] }, action) => {
->>>>>>> feat/loading
   switch (action.type) {
     case START_LOADING:
       return { ...state, isLoading: true };
@@ -40,11 +36,7 @@ export default (state = { isLoading: true, posts: [] }, action) => {
     case FETCH_BY_SEARCH:
       return { ...state, posts: action.payload.data };
     case CREATE:
-<<<<<<< HEAD
       return { ...state, posts: [...state.posts, action.payload] };
-=======
-      return { ...state, posts: action.payload };
->>>>>>> feat/loading
     default:
       return state;
   }
